65218a879f3718469254c691f3081af79bb17af5
潘源 <han@fotome.jp>
2018-06-07 22:32:30 +0900

    初回コミット

---
+++ Assets/bullet.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour {
+    public GameObject explosionPrefab;   //爆発エフェクトのPrefab
+
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, 0.2f, 0);
+
+        if (transform.position.y > 5)
+        {
+            Destroy(gameObject);
+        }
+	}
+    
+    void OnTriggerEnter2D (Collider2D coll) {
+        // 爆発エフェクトを生成する
+        Instantiate (explosionPrefab, transform.position, Quaternion.identity);
+        Destroy (gameObject);
+        Destroy (coll.gameObject);
+    }
+}
---
+++ Assets/rocket.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocket : MonoBehaviour {
+    public GameObject bulletPrefab;
+    private float timeleft;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount > 0) {
+            
+            Touch touch = Input.GetTouch (0);
+        
+            float tx = touch.deltaPosition.x;
+            float sensitivity = 0.2f; // いわゆる感度
+            float mouse_move_x = System.Math.Sign(tx) * sensitivity;
+        
+            transform.Translate(mouse_move_x, 0, 0);
+        }
+    
+        timeleft -= Time.deltaTime;
+        if (timeleft <= 0.0) {
+            //だいたい0.2秒ごとに処理を行う
+            timeleft = 0.2f;
+            Instantiate (bulletPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
